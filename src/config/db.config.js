84324632330ede2be2409745008d30e9d073e7e8
tunji93 <aderintoadetunji@gmail.com>
2023-03-17 23:09:04 +0100

    added cloud build

--- src/config/db.config.js
+++ src/config/db.config.js
@@ -1,15 +1,9 @@
-const {
-    DB_USER,
-    DB_PASSWORD,
-    DB_HOST,
-    DB_NAME,
-    MONGO_DB_PORT
-  } = process.env;
-  
+const { DB_USER, DB_PASSWORD, DB_HOST, DB_NAME, MONGO_DB_PORT } = process.env;
 
-  
+module.exports = {
+  MONGODB_URI: `mongodb+srv://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/${DB_NAME}?retryWrites=true&w=majority`,
+};
 
-  module.exports = {
-    MONGODB_URI: `mongodb://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${MONGO_DB_PORT}/${DB_NAME}?retryWrites=true&w=majority`
-  };
-  
+// module.exports = {
+//   MONGODB_URI: `mongodb://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${MONGO_DB_PORT}/${DB_NAME}?retryWrites=true&w=majority`
+// };
